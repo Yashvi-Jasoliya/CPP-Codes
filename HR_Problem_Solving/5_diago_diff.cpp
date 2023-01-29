4440d06d42f0d994778c0a5854e3b8098de2d421
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-01-29 12:12:44 +0530

    Add files via upload

---
+++ HR_Problem_Solving/5_diago_diff.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+int main()
+{
+int n;
+cin>>n;
+
+int L_diago_sum =0; int R_diago_sum=0;
+int a[n][n];
+for(int i=0; i<n; i++){
+    for(int j=0; j<n ; j++){
+        cin>>a[i][j];
+        if(i==j){
+            L_diago_sum = L_diago_sum +a[i][j];
+        }
+        if(i+j == n-1){
+             R_diago_sum = R_diago_sum +a[i][j];
+        }
+    }
+}
+
+// Addind abs.....
+
+cout<< abs (L_diago_sum - R_diago_sum) <<"\n";
+}
