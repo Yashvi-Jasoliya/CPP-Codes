d6d76a464e29ad2c1333147cf6e6d9cdcf3bc34e
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-01-31 14:15:43 +0530

    Add files via upload

---
+++ HR_Problem_Solving/8_mini-max_sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+int val,i;
+// int n;
+// cin>>n;
+ int sum=0;
+vector<int> array;
+for(i=0; i<5; i++)
+{
+        cin>>val;
+        sum= sum + val;
+        array.push_back(val);
+}
+
+        sort(array.begin(), array.end());   
+        cout<<sum-array[4]<<" "<<sum-array[0];  
+        return 0;
+}
+   
+
+
