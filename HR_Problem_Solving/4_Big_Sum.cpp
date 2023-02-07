badbd78d61fa04ce48264970e7cf67adc38a72cc
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-07 14:02:12 +0530

    Add files via upload

---
+++ HR_Problem_Solving/4_Big_Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int num;
+    long int sum=0;
+    for(int i=0; i<n; i++){
+        cin>>num;
+        sum= sum +num;
+    }
+    cout<<sum;
+}
