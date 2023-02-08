88cb43d61f5359b9e00642b328d2855f63445cb2
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-08 13:04:20 +0530

    Update 5_Diago_Diff.cpp

--- HR_Problem_Solving/5_Diago_Diff.cpp
+++ HR_Problem_Solving/5_Diago_Diff.cpp
@@ -16,18 +16,17 @@ int L_diago_sum =0; int R_diago_sum=0;
 int a[n][n];
     
 for(int i=0; i<n; i++){
-    
-    for(int j=0; j<n ; j++){
+     for(int j=0; j<n ; j++){
         cin>>a[i][j];
+         
         if(i==j){
             L_diago_sum = L_diago_sum +a[i][j];
         }
         if(i+j == n-1){
              R_diago_sum = R_diago_sum +a[i][j];
         }
-    
+         
     }
-
 }
 
 cout<< abs (L_diago_sum - R_diago_sum) <<"\n";
