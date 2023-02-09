230efa749ed8c323609a86e829ef5781f0648515
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-09 14:24:58 +0530

    Update 6_Plus_Minus.cpp

--- HR_Problem_Solving/6_Plus_Minus.cpp
+++ HR_Problem_Solving/6_Plus_Minus.cpp
@@ -13,25 +13,26 @@ int main()
     
  vector<int> arr(n);
     
- for(int arr_i = 0;arr_i < n;arr_i++)
- {
+ for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
         
- if(arr[arr_i]>0)
-        {
+     if(arr[arr_i]>0) {
          p1++;
  }    
-        if(arr[arr_i]<0)
+         if(arr[arr_i]<0)
         {
             n1++;
-     }
+        }
+  
         if(arr[arr_i]==0)
         {
          z1++;
         }
-        }
+  
+  }
         cout<<setprecision(6)<<(float)p1/n<<"\n";
         cout<<setprecision(6)<<(float)n1/n<<"\n";
         cout<<setprecision(6)<<(float)z1/n<<"\n";
+ 
     return 0;
 }
