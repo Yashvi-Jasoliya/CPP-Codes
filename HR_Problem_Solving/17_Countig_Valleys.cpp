58dcca4b31150fd135388c6061786a6bbd626afd
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-09 14:23:13 +0530

    Add files via upload

---
+++ HR_Problem_Solving/17_Countig_Valleys.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int main()
+{
+    int l, count = 0, height = 0;
+    string str;
+    cin >> l;
+    cin >> str;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'D')
+        { // OR height==0;
+            height--;
+        }
+        else if (str[i] == 'U')
+        {
+            height++;
+        }
+        if (str[i] == 'U' && height == 0)
+        {
+            count++;
+        }
+    }
+    cout << count << "\n";
+}
