738861ba68f4811263274496907ebd726015f9fe
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-11 17:33:58 +0530

    Add files via upload

---
+++ HR_Problem_Solving/19_Divisible_Sumpairs.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+
+    int n, k;
+    int count = 0;
+
+    cin >> n >> k;
+
+    vector<int> array(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i < j)
+            {
+                if ((array[i] + array[j]) % k == 0)
+                    count++;
+            }
+        }
+    }
+    cout << count;
+
+    return 0;
+}
