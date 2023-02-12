7834b5911a905f226db88220c21bda415f9b939b
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-12 19:16:24 +0530

    Add files via upload

---
+++ HR_Problem_Solving/9_Cake_Candles.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+    int n, max = 0, can, a;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+
+        if (a > max)
+        {
+            max = a;
+            can = 1;
+        }
+        else if (a == max)
+            can++;
+    }
+
+    cout << can;
+
+    return 0;
+}
