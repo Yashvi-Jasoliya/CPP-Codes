5093188917c12533bc1ab79b1dd944f6df27b609
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-15 21:18:42 +0530

    Add files via upload

---
+++ HR_Problem_Solving/22_Sales_By_Match.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+vector<string> split(const string &);
+#define FI(i, a, b) for (int i = (a); i <= (b); i++)
+#define FD(i, a, b) for (int i = (a); i >= (b); i--)
+
+#define PI 3.14;
+
+using namespace std;
+
+int n, a[105], x;
+
+int main()
+{
+
+    scanf("%d", &n);
+    FI(i, 1, n)
+    {
+        scanf("%d", &x);
+        a[x]++;
+    }
+
+    int ans = 0;
+
+    FI(i, 1, 100)
+    ans = ans + a[i] / 2;
+    printf("%d\n", ans);
+    return 0;
+}
