3a3df9d8faa869f61cd82b41f2905a89988c1447
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-21 18:49:12 +0530

    Add files via upload

---
+++ HR_Problem_Solving/28_Hurdle_Race.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+    int n, k;
+    
+    int max_A = 0;
+    
+    cin >> n >> k;
+    
+    vector<int> height(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> height[i];
+        max_A = max(max_A, height[i]);
+    }
+   
+    cout << max(0, max_A - k);
+
+    return 0;
+}
