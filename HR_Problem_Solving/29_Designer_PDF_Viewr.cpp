780c6208db7c9d2ead44c3ebbee838a00bd279e6
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-22 21:12:25 +0530

    Add files via upload

---
+++ HR_Problem_Solving/29_Designer_PDF_Viewr.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+    string s;
+    int height[50];
+
+    for (int i = 0; i < 26; i++)
+    {
+        cin >> height[i];
+    }
+    cin >> s;
+
+    int len, hei = 0;
+    len = s.length();
+
+    int temp;
+    int var;
+
+    for (int i = 0; i < len; i++)
+    {
+        char ch = s[i];
+        
+        temp = int(ch);
+        temp = temp - 97;
+        var = height[temp];
+
+        if (var >= hei)
+        {
+            hei = var;
+        }
+    }
+
+    cout << len * hei << "\n";
+}
