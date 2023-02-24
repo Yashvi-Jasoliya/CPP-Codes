b55a10b658dd68c9e4db80a6f7cd031abc8753f0
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-24 21:09:26 +0530

    Update 30_Utopian_Tree.cpp

--- HR_Problem_Solving/30_Utopian_Tree.cpp
+++ HR_Problem_Solving/30_Utopian_Tree.cpp
@@ -35,7 +35,7 @@ int main()
                 {
                     height = height + 1;
                     sum = 0;
-                    spr = 1
+                    spr = 1;
                 }
            }
             
