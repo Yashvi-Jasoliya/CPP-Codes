891b6287c929a2909303adc7cd9e5f947e1f5cc9
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-02-27 21:29:23 +0530

    Add files via upload

---
+++ HR_Problem_Solving/31_Angry_Professor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+    
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int x, y, k = 0;
+        cin >> x >> y;
+
+        for (int i = 0; i < x; i++)
+        {
+            int a_t;
+            cin >> a_t;
+
+            if (a_t <= 0)
+            {
+                k++;
+            }
+        }
+
+        if (k >= y)
+        {
+            cout << "NO" << '\n';
+        }
+        else
+        {
+            cout << "YES" << '\n';
+        }
+    }
+
+    return 0;
+}
