015175931faae47bace7d273bf10ad72544a13e3
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-03-03 19:52:16 +0530

    Add files via upload

---
+++ HR_Problem_Solving/35_Circular_Array_Rotation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+
+    int n, q, k;
+    cin >> n >> q >> k;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    k %= n;
+
+    for (int i = 0; i < q; i++)
+    {
+        int b;
+        cin >> b;
+
+        cout << arr[(n - k + b) % n] << "\n";
+    }
+    return 0;
+}
