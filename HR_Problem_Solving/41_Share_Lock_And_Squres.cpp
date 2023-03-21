1a1bac3688b3de01575ac33840af0fc12dd44c4d
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-03-21 21:20:54 +0530

    Add files via upload

---
+++ HR_Problem_Solving/41_Share_Lock_And_Squres.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main()
+{
+    int n, a, b;
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> a >> b;
+
+        int q = (int)sqrt(b) - (int)sqrt(a);
+        
+        if (sqrt(a) - (int)sqrt(a) == 0)
+            q++;
+
+        cout << q << "\n";
+    }
+    return 0;
+}
